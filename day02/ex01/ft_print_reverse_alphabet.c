c55f400c0cbf2b5ed25cdc2fe79a3199585deb0d
LINDOKUHLE SIBEKO <lsibeko@c5r3s11.wethinkcode.co.za>
2020-06-21 10:53:39 +0200

    done

--- day02/ex01/ft_print_reverse_alphabet.c
+++ day02/ex01/ft_print_reverse_alphabet.c
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsibeko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/20 09:28:56 by lsibeko           #+#    #+#             */
-/*   Updated: 2020/06/21 10:40:07 by lsibeko          ###   ########.fr       */
+/*   Created: 2020/06/20 10:43:50 by lsibeko           #+#    #+#             */
+/*   Updated: 2020/06/21 10:41:39 by lsibeko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void 	ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	char i;
 
-	i = 'a';
-	while (i <= 'z' )
+	i = 'z';
+	while (i >= 'a' )
 	{
 		ft_putchar(i);
-		i++;
+		i--;
 	}
 }
